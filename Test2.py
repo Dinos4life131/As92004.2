bedaf08fcb3b8d693deeba2596760a6f362cd75d
Dinos4life131 <221052@trinityschools.nz>
2024-05-29 12:44:40 +1200

    Asking user how many rounds they want

--- Test2.py
+++ Test2.py
@@ -1,1 +1,16 @@
-print("WORLD2")
+def print_rounds_function():
+    while True:
+        number_of_rounds = input("Enter the number of rounds you want to play (press Enter for infinite): ")
+        
+        if number_of_rounds == "":
+            infinite_rounds = True
+            print("Infinite rounds it is!")
+            break
+        elif number_of_rounds.isdigit():
+            num_rounds = int(number_of_rounds)
+            infinite_rounds = False
+            print(f"{num_rounds} rounds it is!")
+            break
+ 
+if __name__ == "__main__":
+    print_rounds_function()
